c33af8b01e5704a3ca8af743f19cfda174e9da79
Christopher-Lamb <70704179+Christopher-Lamb@users.noreply.github.com>
2024-05-17 08:30:56 -0400

    mainifest with images

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,36 @@ module.exports = {
     title: `snake-game`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: `Snake Game`,
+        short_name: `Snake Game`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: `${__dirname}/src/images/favicon.png`,
+        icons: [
+          {
+            src: `${__dirname}/src/images/favicon.png`,
+            sizes: `64x64 32x32 24x24 16x16`,
+            type: `image/png`,
+          },
+          {
+            src: `${__dirname}/src/images/192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `${__dirname}/src/images/512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ],
 };
